df74a4e59b5f9655d409673311fa74199417f933
Diasfu <diasfu@gmail.com>
2017-03-01 23:47:36 +0000

    First and second page are almost done. Modified the emptyActivity, formated fields. Method of the scroll next animal is there, but no sure if working

--- app/src/main/java/com/example/android/bfframe/AnimalPick.java
+++ app/src/main/java/com/example/android/bfframe/AnimalPick.java
@@ -3,11 +3,11 @@ package com.example.android.bfframe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CatActivity extends AppCompatActivity {
+public class AnimalPick extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cat);
+        setContentView(R.layout.activity_animal_pick);
     }
 }
--- app/src/main/java/com/example/android/bfframe/DogActivity.java
+++ app/src/main/java/com/example/android/bfframe/DogActivity.java
@@ -1,18 +1,21 @@
 package com.example.android.bfframe;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 
-/**
- * Created by diasf on 27/02/2017.
- */
+public class DogActivity extends AppCompatActivity {
 
-public class DogActivity {
-
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog);
+    }
 }
+
+
 class LearnGesture extends GestureDetector.SimpleOnGestureListener{
 
     public boolean onFling(MotionEvent eventNext, MotionEvent eventBack, float velocityX, float velocityY){
@@ -24,8 +27,8 @@ class LearnGesture extends GestureDetector.SimpleOnGestureListener{
         if(eventBack.getX() < eventNext.getX()){
             //right to left swipe
 
-            }
-            return true;
+        }
+        return true;
 
     }
 }
